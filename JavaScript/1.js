9ffc0d0eb195c5002e43e2c1402f119fb310239d
rlawhdwls <50696204+rlawhdwls@users.noreply.github.com>
2021-04-19 01:32:32 +0900

    210419-1
    210419-1
    반응형탭 구현x
    계획짜기 o

--- JavaScript/1.js
+++ JavaScript/1.js
@@ -1,11 +1,77 @@
 'use strict';
 
-$('.script3').text('I write this letter to JS');
+/*  //---상속---
+// 부모클래스
+let Person =function(){};
+Person.prototype={
+    eat:function(){console.log('우물우물');}
+};
 
-
-document.('.script3').write('asdf');
-
-function Dialog1(){
-    document.alert('asdf');
+// 자식클래스
+let BusinessPerson = function(){};
+// 상속
+BusinessPerson.prototype = new Person();
+//오버로딩
+BusinessPerson.prototype.work=function(){
+    console.log('먹보');
 }
+
+// 클래스 인스턴스 생성 -> 자식클래스
+let func = new BusinessPerson();
+func.eat();
+func.work();
+//부모클래스
+func=new Person();
+func.eat();
+func.work();  //error
+*/
+
+/*
+//---private---
+let Person = function(name,birth){
+    let _name = name;
+    let _birth = birth;
+
+    this.getName = function(){
+        return _name;
+    }
+    this.getBirth = function(){
+        return _birth;
+    }
+}
+
+Person.prototype.toString = function(){
+    return 'Person : ' + this.getName() + ' ' + this.getBirth();
+}
+
+let p = new Person('종진',961206);
+console.log( p.toString());
+
+let str = p;
+alert(str);
+*/
+/*
+// 네임스페이스
+var Info = Info || {};
+Info.Person= function(name,birth){
+    this.name=name;
+    this.birth=birth;
+};
+
+Info.Person.prototype.toString=function(){
+    return this.name + ' _ _ ' + this.birth;
+}
+let p = new Info.Person('김종진',new Date(1996,12,6));
+console.log(p.toString());
+*/
+ 
+
+
+
+
+
+
+
+
+
 
