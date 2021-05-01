9132cb43b968ac273268550b8eb1cb7b73235ace
rlawhdwls <50696204+rlawhdwls@users.noreply.github.com>
2021-05-01 17:15:06 +0900

    210501-1
    210501-1
    Upload

---
+++ web공부/JavaScript/Home/clock.js
@@ -0,0 +1,18 @@
+const clock = document.querySelector('.js-clock');
+console.log('asdf')
+function Clock_play(){
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    const time = `${hour<10? `0${hour}` : `${hour}`} : ${minute<10? `0${minute}` : `${minute}`} : ${seconds<10 ? `0${seconds}` : `${seconds}`}`
+
+
+    clock.innerText = time;
+}
+
+function init(){
+    setInterval(Clock_play,1000);
+}
+init();
